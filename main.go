20cc15424b61f33f6cf44e3fed458c63bdfb764a
souvikmaji <souvikmaji94@gmail.com>
2019-05-12 13:29:04 +0530

    error handling if no filename given

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
 )
 
 func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "no filename given\n")
+		os.Exit(-1)
+	}
+
 	fileName := os.Args[1]
 	code, err := ioutil.ReadFile(fileName)
 	if err != nil {
